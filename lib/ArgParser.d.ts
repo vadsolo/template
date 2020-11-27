301620650786ca81c6bc3619cab0c77bd14335ab
Vadim Solo <vadsolo.dev@gmail.com>
2020-11-27 12:00:09 +0200

    lib to gitignore

--- lib/ArgParser.d.ts
+++
@@ -1,7 +0,0 @@
-import { DefaultArgOptions } from "./Options";
-export declare class ArgParser {
-    private options;
-    constructor(rawArgs: string[]);
-    private parseArguments;
-    get getOptions(): DefaultArgOptions;
-}
--- lib/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function cli(rawArgs: string[]): Promise<void>;
